a2ec8728072dcb5f73a8157f9049d5b32e06b8d7
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-05-23 12:35:02 +0000

    Add datatypes.js with basic data type examples

---
+++ Basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict";// treat all js code as newer version
+
+let age = 18
+let valuecorrect = true
+
+
+// 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null = standalone value
+// unidentified
+// symbol=> unique
+
+// object
+
+console.log(typeof null) // object
