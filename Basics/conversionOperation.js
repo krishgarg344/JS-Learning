7f0b6ecbe16439a183e2319e30dd6687a4fc2d6a
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-05-24 05:39:27 +0000

    Add conversionOperation.js with basic data type examples

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,11 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(score)
+
+console.log(typeof Number(score))
+console.log(score)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
