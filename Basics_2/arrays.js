ef6edf79604f0ac182a5891b0354695e0cfd286a
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-06-07 09:47:29 +0000

    arrays updated

--- Basics_2/arrays.js
+++ Basics_2/arrays.js
@@ -8,3 +8,25 @@ const fruits = [];
 fruits.push("apple", "banana", "kiwi");
 console.log(fruits.length);
 
+// Array methods
+
+const myheros = ["tony stark", "steve rogers", "natasha", "dr banner"]
+myheros.push("peter parker")
+console.log(myheros);
+myheros.pop()
+console.log(myheros);
+
+// slice, splice
+
+console.log("A ", myheros);
+const arr1 = myheros.slice(1, 3)
+console.log(arr1);
+
+console.log("B ", myheros);
+const arr2 = myheros.splice(1, 3)
+console.log(arr2);
+console.log(myheros);
+
+
+const trash = myheros.concat(fruits)
+console.log(trash);
