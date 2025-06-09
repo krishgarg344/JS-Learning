232d31d568ce8b7d9336771c140e33dc82c551ef
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-06-09 11:25:51 +0000

    Objects one updated

--- Basics_2/objects_1.js
+++ Basics_2/objects_1.js
@@ -16,3 +16,18 @@ console.log(JsUser.email);
 console.log(typeof mySym);
 console.log(JsUser.lastOnline[1]);
 console.log(JsUser[mySym]);
+
+JsUser.email = "userAiExpert@gmail.com"
+// Object.freeze(JsUser)
+JsUser.email = "noExpert@yahoo.com"
+console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello Shawty");
+}
+console.log(JsUser.greeting());
+
+JsUser.greetingtwo = function(){
+    console.log(`Hello Shawty, ${this.name} here`);
+}
+console.log(JsUser.greetingtwo());
