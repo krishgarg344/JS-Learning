886b92be401ed2f77a63d91aa7632b1aa46cdfae
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-06-11 16:40:44 +0000

    Functions updated

---
+++ Basics_3/functions.js
@@ -0,0 +1,12 @@
+function addNums(num1, num2){
+    console.log(num1 + num2);    
+}
+
+console.log(addNums(21, 5));
+
+function addTwoNums(num1, num2){
+    let result = num1 + num2
+    return result
+}
+
+console.log("Sum :",addTwoNums(2, 7));
