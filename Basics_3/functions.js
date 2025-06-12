00360ddc80cbde3f06da080961afa5c9f002261b
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-06-12 09:49:08 +0000

    Functions updated

--- Basics_3/functions.js
+++ Basics_3/functions.js
@@ -10,3 +10,23 @@ function addTwoNums(num1, num2){
 }
 
 console.log("Sum :",addTwoNums(2, 7));
+
+function greeting(user){
+    if(user === undefined){
+        console.log("Please enter a username");
+        return
+    }
+    return `${user}, hello sir !!`
+}
+
+console.log(greeting());
+
+const info = {
+    username : "use_urr",
+    contact : 9999990007
+}
+
+function handleIt(usr_info) {
+    console.log(`Username : ${usr_info.username},\nContact : ${usr_info.contact}`);    
+}
+handleIt(info)
