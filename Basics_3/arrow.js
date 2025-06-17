1ab1bd796cc4a6644fe23bc240d9b93e595fe39c
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-06-17 07:28:47 +0000

    arrow done

--- Basics_3/arrow.js
+++ Basics_3/arrow.js
@@ -12,3 +12,26 @@ const user = {
 user.welcome()
 user.name = "sam"
 user.welcome()
+
+// 'this' only works for an object not ona a function
+
+const one = function(){
+    let height = "six feet"
+    console.log("height :", this.height);
+}
+one()
+
+// arrow function
+const two = () => {
+    let height = "six feet"
+    console.log("height :", this.height);
+}
+two()
+
+// Syntax of arrow function : () => {}
+
+const addnums = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addnums(3,7));
+ 
