0571cc57eba7b40054a7d7ad8c9b05b275e2442e
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-06-17 08:06:54 +0000

    IIFE done

--- Basics_3/IIFE.js
+++ Basics_3/IIFE.js
@@ -4,6 +4,10 @@
 
 // IIFE is used not only for immediate execution of function but maily to reduce pollution for global scope
 
-(function message() {
+(function message() {  // named IIFE
     console.log(`DB COnnected`);
-})()
+})(); // always end IIFE with ; 
+
+((name) => {    // unnamed IIFE
+    console.log(`DB connected again ${name}`);
+})("frank");
