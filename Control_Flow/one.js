69fedd2e76ea854f6f22808b846b5fab6bf910fe
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-06-18 16:56:13 +0000

    one updated

---
+++ Control_Flow/one.js
@@ -0,0 +1,16 @@
+// if
+
+// == : checks equality while === : checks equality with typeof
+
+if (2 == "2") {
+    console.log("yess");
+}
+
+if (2 === "2") {
+    console.log("yess");
+}
+else {
+    console.log("false");
+}
+
+// && : and , || : or for more than one conditions
