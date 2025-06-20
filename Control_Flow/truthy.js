38cb6ea9538be9ba56afa62e05781135a06e49d4
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-06-20 09:22:08 +0000

    Truthy Done

--- Control_Flow/truthy.js
+++ Control_Flow/truthy.js
@@ -16,6 +16,17 @@ else {
 
 const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) { // using Object.keys accesses keys as array and then wwe can operate .length on it
+if (Object.keys(emptyObj).length === 0) { // using Object.keys accesses keys of object as array and then wwe can operate .length on it
     console.log("empty Object");  
 }
+
+// Nullish Coalescing Operator (??): null undefined
+// let val1 = 5 ?? 10
+let val1 = null ?? 3
+console.log(val1);
+
+// Terniary Operator
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice > 80 ? console.log("greater than 80") : console.log("less than 80")
