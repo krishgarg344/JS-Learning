5461dde03cdc6439f06e05dcb3fc7403d797d3e5
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-06-23 07:54:20 +0000

    for of updated

--- Iterations/for_of.js
+++ Iterations/for_of.js
@@ -13,3 +13,32 @@ const greet = "Heyy How u doing"
 for (const g of greet) {
     console.log(g);
 }
+
+// Maps
+
+const x = new Map()
+x.set('IN', 'India')
+x.set('UK', 'United Kingdom')
+x.set('SA', 'South Africa')
+x.set('IN', 'India')
+console.log(x);// Always contains unique values
+
+for (const [key, value] of x) {
+    console.log(key, ':-', value);
+}
+
+const Obj = {
+    game1 : 'NFS', 
+    game2 : 'GTA V',
+    game3 : 'COD'
+}
+
+// for (const key of Obj) {
+//     console.log(key);
+// } objects are not iterable
+
+// for in 
+for (const key in Obj) {
+   console.log(key, ":-", Obj[key]);
+}
+ 
