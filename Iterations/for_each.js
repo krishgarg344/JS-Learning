ec5cca1993116672081123023081d4168499edcd
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-06-29 19:49:30 +0000

    for

--- Iterations/for_each.js
+++ Iterations/for_each.js
@@ -3,10 +3,14 @@ const langs = ["js", "cpp", "ruby"]
 //     console.log(val);
 // })
 
-langs.forEach((key) => {
-    console.log(key);
-})
+// langs.forEach((key) => {
+//     console.log(key);
+// })
 
-langs.forEach( (val, index, arr) => {
-    console.log(val, index, arr);
-})
+// langs.forEach( (val, index, arr) => {
+//     console.log(val, index, arr);
+// }) 
+
+const nums = [11, 22, 33, 44, 55, 66, 77, 88, 99]
+const val = nums.filter((key) => key > 40)
+console.log(val);
