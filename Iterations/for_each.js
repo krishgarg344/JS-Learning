3c83202da5160c2c55b4a29c386dfb7dc11439b7
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-07-06 18:52:25 +0000

    for each updated

--- Iterations/for_each.js
+++ Iterations/for_each.js
@@ -12,20 +12,38 @@ const langs = ["js", "cpp", "ruby"]
 // }) 
 
 const nums = [11, 22, 33, 44, 55, 66, 77, 88, 99]
-const val = nums.filter((key) => key > 40)
-console.log(val);
+// const val = nums.filter((key) => key > 40)
+// console.log(val);
 
-// or
+// // or
 
-let newnums = []
-nums.forEach( (key) => {
-    if (key > 40) {
-        newnums.push(key)
-    }
-})
+// let newnums = []
+// nums.forEach( (key) => {
+//     if (key > 40) {
+//         newnums.push(key)
+//     }
+// })
+// // console.log(newnums);
+
+// newnums = nums.filter( (k) => {
+//     return nums > 30 && nums < 90
+// })
 // console.log(newnums);
 
-newnums = nums.filter( (k) => {
-    return nums > 30 && nums < 90
-})
+// Map
+
+const newnums = nums
+                    .map( (key) => key * 10)
+                    .map( (key) => key + 1)
+                    .filter( (key) => key >= 400)
+
 console.log(newnums);
+
+const int = [1, 2, 3]
+
+const sum = int.reduce(function (acc, curr) {
+    console.log(`accumulator : ${acc} and current value : ${curr}`);
+    return acc + curr
+}, 0)
+
+console.log(sum)
