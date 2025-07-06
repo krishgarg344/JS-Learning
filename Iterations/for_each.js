0d33993f282b993f4c823259dc2c92b47cccfb46
krishgarg344 <68385843+krishgarg344@users.noreply.github.com>
2025-07-06 18:12:25 +0000

    for each updated

--- Iterations/for_each.js
+++ Iterations/for_each.js
@@ -14,3 +14,18 @@ const langs = ["js", "cpp", "ruby"]
 const nums = [11, 22, 33, 44, 55, 66, 77, 88, 99]
 const val = nums.filter((key) => key > 40)
 console.log(val);
+
+// or
+
+let newnums = []
+nums.forEach( (key) => {
+    if (key > 40) {
+        newnums.push(key)
+    }
+})
+// console.log(newnums);
+
+newnums = nums.filter( (k) => {
+    return nums > 30 && nums < 90
+})
+console.log(newnums);
